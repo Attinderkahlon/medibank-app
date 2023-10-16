cf7232faa5e3b3851865a65573aa59060e45578b
Attinder <105198941+Attinderkahlon@users.noreply.github.com>
2023-10-17 09:40:44 +1100

    feat(layout): add component layout added

---
+++ src/components/layout/index.tsx
@@ -0,0 +1,15 @@
+import { ReactNode } from 'react'
+import Header from './Header'
+
+type LayoutProps = {
+  children: ReactNode
+}
+
+export default function Layout({ children }: LayoutProps) {
+  return (
+    <div className="max-w-3xl mx-auto px-4">
+      <Header />
+      <div>{children}</div>
+    </div>
+  )
+}
