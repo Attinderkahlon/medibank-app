ab25078d21a6a88402bb62956b70cd1747d0a0af
Attinder <105198941+Attinderkahlon@users.noreply.github.com>
2023-10-17 09:40:44 +1100

    component(header): add basic Header component

---
+++ src/components/layout/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <header className="shadow p-3">
+      <img alt="medibank" src="/medibank.webp" className="object-cover h-full mx-auto" />
+    </header>
+  )
+}
